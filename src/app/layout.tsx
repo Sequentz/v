6a35e5f74073b101ba7dbe0cd111c720ebc90de9
Sequentz <wouterkvandenbossche@gmail.com>
2024-06-24 15:25:19 +0200

    next font local

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,10 +4,11 @@ import "./globals.css";
 import { CartProvider } from "@/components/CartContext";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
-import localFont from 'next/font/local'
+import localFont from 'next/font/local';
+
  
 const myFont = localFont({
-  src: './OPTIBankGothic-Medium.otf',
+  src: '../../public/OPTIBankGothic-Medium.otf',
   display: 'swap',
 })
 const inter = Inter({ subsets: ["latin"] });
